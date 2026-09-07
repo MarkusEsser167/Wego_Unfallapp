9e4907596b167294014c9c2c82ea9af9be2a9e57
Markus Esser <markus@esser-soehne.de>
2026-09-07 11:42:06 +0200

    Standortverteiler aus ndl.json, Vertriebsleiter zusaetzlich in CC

--- sw.js
+++ sw.js
@@ -1,10 +1,11 @@
-const CACHE = 'wego-unfallapp-v1';
+const CACHE = 'wego-unfallapp-v2';
 const FILES = [
   './',
   './index.html',
   './manifest.json',
   './icon-192.png',
-  './icon-512.png'
+  './icon-512.png',
+  './ndl.json'
 ];
 
 self.addEventListener('install', e => {
@@ -22,6 +23,18 @@ self.addEventListener('activate', e => {
 });
 
 self.addEventListener('fetch', e => {
+  // Standortliste immer zuerst aus dem Netz holen, damit Aenderungen
+  // an ndl.json ohne neue App-Version ankommen; offline aus dem Cache.
+  if (e.request.url.indexOf('ndl.json') !== -1) {
+    e.respondWith(
+      fetch(e.request).then(r => {
+        const copy = r.clone();
+        caches.open(CACHE).then(c => c.put(e.request, copy));
+        return r;
+      }).catch(() => caches.match(e.request))
+    );
+    return;
+  }
   e.respondWith(
     caches.match(e.request).then(r => r || fetch(e.request))
   );
